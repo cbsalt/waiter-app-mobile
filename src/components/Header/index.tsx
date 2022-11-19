417ecd88780c18ca0fe2e8539545067b249d811a
Charles <cbsal.tec@gmail.com>
2022-11-18 22:49:55 -0300

    feat: create Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,45 @@
-import { CustomText } from '../CustomText';
-import { Container } from './styles';
+import { TouchableOpacity } from 'react-native';
 
-export function Header() {
+import { CustomText } from '../CustomText';
+
+import { Container, OrderContent, OrderHeader, Table } from './styles';
+
+interface HeaderProps {
+  selectedTable: string;
+  onCancelOrder: () => void;
+}
+
+export function Header({ selectedTable, onCancelOrder }: HeaderProps) {
   return (
     <Container>
-      <CustomText size={14} opacity={0.9}>Bem-Vindo(a) ao</CustomText>
-      <CustomText size={24} weight="700">
-        WAITER
-        <CustomText size={24}>APP</CustomText>
-      </CustomText>
+      {!selectedTable && (
+        <>
+          <CustomText size={14} opacity={0.9}>Bem-Vindo(a) ao</CustomText>
+          <CustomText size={24} weight="700">
+            WAITER
+            <CustomText size={24}>APP</CustomText>
+          </CustomText>
+        </>
+      )}
+
+      {selectedTable && (
+        <OrderContent>
+          <OrderHeader>
+            <CustomText size={24} weight="600">Pedido</CustomText>
+            <TouchableOpacity onPress={onCancelOrder}>
+              <CustomText color='#d73035' weight='600' size={14}>
+              cancelar pedido
+              </CustomText>
+            </TouchableOpacity>
+          </OrderHeader>
+
+          <Table>
+            <CustomText color='#666'>
+              Mesa {selectedTable}
+            </CustomText>
+          </Table>
+        </OrderContent>
+      )}
     </Container>
   );
 }
