d509f552d44d441f1bd9dcf2c40810ac2235fa28
Charles <cbsal.tec@gmail.com>
2022-11-19 16:16:15 -0300

    feat: create order confirmation

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -4,6 +4,7 @@ import { FlatList, TouchableOpacity } from 'react-native';
 import { CartItem } from '../../types/CartItem';
 import { Product } from '../../types/Products';
 import { formatCurrency } from '../../utils';
+import { api } from '../../utils/api';
 
 import { Button } from '../Button';
 import { CustomText } from '../CustomText';
@@ -24,12 +25,13 @@ import {
 
 interface CartProps {
   cartItems: CartItem[];
-  onAdd: (product: Product) => void
-  onDecrement: (product: Product) => void
-  onConfirmOrder: () => void
+  onAdd: (product: Product) => void;
+  onDecrement: (product: Product) => void;
+  onConfirmOrder: () => void;
+  selectedTable: string;
 }
 
-export function Cart({ cartItems, onAdd, onDecrement, onConfirmOrder }: CartProps) {
+export function Cart({ cartItems, onAdd, onDecrement, onConfirmOrder, selectedTable }: CartProps) {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -37,7 +39,19 @@ export function Cart({ cartItems, onAdd, onDecrement, onConfirmOrder }: CartProp
     return acc + cartItem.quantity * cartItem.product.price;
   }, 0);
 
-  function handleConfirmOrder() {
+  async function handleConfirmOrder() {
+    setIsLoading(true);
+
+    const payload = {
+      table: selectedTable,
+      products: cartItems.map((cartItem) => ({
+        product: cartItem.product._id,
+        quantity: cartItem.quantity
+      }))
+    };
+
+    await api.post('/orders', payload);
+    setIsLoading(false);
     setIsModalVisible(true);
   }
 
