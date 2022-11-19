c943c48bc59240fc9bf13284e0449eee2b661dd3
Charles <cbsal.tec@gmail.com>
2022-11-18 22:56:06 -0300

    feat: create Main styles

--- src/Main/styles.ts
+++ src/Main/styles.ts
@@ -17,11 +17,8 @@ export const MenuContainer = styled.View`
   flex: 1;
 `;
 
-export const Footer = styled.View`
-  min-height: 110px;
-  background: #fff;
-  padding: 16px 24px;
-`;
-
-export const FooterContainer = styled.SafeAreaView`
+export const CenteredContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex: 1;
 `;
