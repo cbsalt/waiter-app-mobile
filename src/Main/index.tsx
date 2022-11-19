6104ffbe36cd1a8914fbad8561a918aadbf6fc1f
Charles <cbsal.tec@gmail.com>
2022-11-18 22:55:50 -0300

    feat: reset, add and decrement functions

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,43 +1,144 @@
 import { useState } from 'react';
+import { ActivityIndicator } from 'react-native';
+
 import { Button } from '../components/Button';
+import { Cart } from '../components/Cart';
 import { Categories } from '../components/Categories';
+import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import { Menu } from '../components/Menu';
 import { TableModal } from '../components/TableModal';
-import { CategoriesContainer, Container, Footer, FooterContainer, MenuContainer } from './styles';
+import { CartItem } from '../types/CartItem';
+import { Product } from '../types/Products';
+
+import { CategoriesContainer, CenteredContainer, Container, MenuContainer } from './styles';
+
+import { products as mockProducts } from '../mocks/products';
+import { Empty } from '../components/Icons/Empty';
+import { CustomText } from '../components/CustomText';
 
 export function Main() {
   const [isTableModalVisible, setIsTableModalVisible] = useState(false);
   const [selectedTable, setSelectedTable] = useState('');
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [products] = useState<Product[]>(mockProducts);
 
   function handleSaveTable(table: string) {
     setSelectedTable(table);
   }
 
+  function handleResetOrder() {
+    setSelectedTable('');
+    setCartItems([]);
+  }
+
+  function handleAddToCart(product: Product) {
+    if (!selectedTable) {
+      setIsTableModalVisible(true);
+    }
+
+    setCartItems((prevState) => {
+      const itemIndex = prevState.findIndex(cartItem => cartItem.product._id === product._id);
+
+      if (itemIndex < 0) {
+        return prevState.concat({
+          quantity: 1,
+          product
+        });
+      }
+
+      const newCartItems = [...prevState];
+      const item = newCartItems[itemIndex];
+
+      newCartItems[itemIndex] = {
+        ...item,
+        quantity: item.quantity + 1
+      };
+
+      return newCartItems;
+    });
+  }
+
+  function handleDecrementCartItem(product: Product) {
+    setCartItems((prevState) => {
+      const itemIndex = prevState.findIndex(cartItem => cartItem.product._id === product._id);
+      const item = prevState[itemIndex];
+      const newCartItems = [...prevState];
+
+      if (item.quantity === 1) {
+        const newCartItems = [...prevState];
+        newCartItems.splice(itemIndex, 1);
+
+        return newCartItems;
+      }
+
+
+      newCartItems[itemIndex] = {
+        ...item,
+        quantity: item.quantity - 1
+      };
+
+      return newCartItems;
+    });
+  }
+
   return (
     <>
       <Container>
-        <Header />
+        <Header
+          selectedTable={selectedTable}
+          onCancelOrder={handleResetOrder}
+        />
 
-        <CategoriesContainer>
-          <Categories />
-        </CategoriesContainer>
+        {isLoading && (
+          <CenteredContainer>
+            <ActivityIndicator color="#d73035" size="large" />
+          </CenteredContainer>
+        )}
 
-        <MenuContainer>
-          <Menu />
-        </MenuContainer>
+        {!isLoading && (
+          <>
+            <CategoriesContainer>
+              <Categories />
+            </CategoriesContainer>
 
+            {products.length > 0 ? (
+              <MenuContainer>
+                <Menu
+                  products={products}
+                  onAddToCart={handleAddToCart}
+                />
+              </MenuContainer>
+            ) : (
+              <CenteredContainer>
+                <Empty />
+
+                <CustomText color="#666" style={{ marginTop: 24 }}>
+                  Nenhum produto foi encontrado!
+                </CustomText>
+              </CenteredContainer>
+            )}
+
+          </>
+        )}
       </Container>
 
       <Footer>
-        <FooterContainer>
-          {!selectedTable && (
-            <Button onPress={() => setIsTableModalVisible(true)}>
+        {!selectedTable && (
+          <Button disabled={isLoading} onPress={() => setIsTableModalVisible(true)}>
             Novo Pedido
-            </Button>
-          )}
+          </Button>
+        )}
 
-        </FooterContainer>
+        {selectedTable && (
+          <Cart
+            cartItems={cartItems}
+            onAdd={handleAddToCart}
+            onDecrement={handleDecrementCartItem}
+            onConfirmOrder={handleResetOrder}
+          />
+        )}
       </Footer>
 
       <TableModal
