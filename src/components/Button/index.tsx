af95cfe810cff0ec04aba7375779876be49a7c2d
Charles <cbsal.tec@gmail.com>
2022-11-18 22:51:22 -0300

    feat: conditional rendering

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,27 @@
+import { ActivityIndicator } from 'react-native';
 import { CustomText } from '../CustomText';
+
 import { Container } from './styles';
 
 interface ButtonProps {
   children: string;
   onPress: () => void;
   disabled?: boolean;
+  loading?: boolean;
 }
 
-export function Button({ children, onPress, disabled }: ButtonProps) {
+export function Button({ children, onPress, disabled, loading }: ButtonProps) {
   return (
-    <Container onPress={onPress} disabled={disabled}>
-      <CustomText weight="600" color="#fff">
-        {children}
-      </CustomText>
+    <Container onPress={onPress} disabled={disabled || loading}>
+      {!loading && (
+        <CustomText weight="600" color="#fff">
+          {children}
+        </CustomText>
+      )}
+
+      {loading && (
+        <ActivityIndicator color="#fff" />
+      )}
     </Container>
   );
 }
